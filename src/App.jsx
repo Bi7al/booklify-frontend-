9dda961d2cb27ada1569d16bd9b5f818ff9af30d
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2026-02-19 00:37:00 +0500

    Booklify Frontend v 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 
 export default function App() {
 
-
-
-
   const [results, setResults] = useState([]);
+  const [preview, setPreview] = useState(null);
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setPreview(url);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,47 +26,54 @@ export default function App() {
     const data = await res.json();
     setResults(data);
   };
+
+
+
   return (
-    // Two Sections: 1.Upload Window, 2. Interest Window, 3. Result Window
+    <section className='h-screen w-full'>
+      <nav className='p-8 bg-orange-950'><h1 className='text-white font-extrabold text-4xl'>Booklify</h1></nav>
+      <form onSubmit={handleSubmit} className='h-4/5 w-full p-10 flex'>
+        {/* Input Picture */}
 
+        {
+          preview ? <img src={preview} alt="Preview" className="w-1/2 h-full rounded-2xl " /> : <label className="w-1/2 h-full border text-xl border-white bg-zinc-900 hover:bg-zinc-800 rounded-2xl p-4 text-white cursor-pointer flex items-center justify-center">
+            Choose Image
+            <input
+              name='uploadedImage'
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+          </label>
+        }
+        <div className='w-1/2 h-full'>
+          {/* Interest Section */}
+          <div className='w-full h-1/3 flex flex-col gap-4 ps-4'>
+            <textarea name="interests" id="" placeholder='Enter interests seperated by Comma.' className='border rounded-2xl p-4 h-full' />
+            <button className='w-fit px-6 py-2 bg-orange-900 text-white font-bold rounded-lg hover:bg-blue-500 cursor-pointer'>Submit</button>
+          </div>
+          <div className='ps-4 pt-4 h-1/2'>
+            {/* Result Window */}
+            <h1>Results:</h1>
+            <hr />
+            <div className='h-full overflow-y-auto'>
+              {
+                results.length > 0 ? results.map((result, index) => {
+                  return (
+                    <div key={index} className='border rounded-lg p-4 my-4'>
+                      <h2 className='text-xl font-bold'>{result.title}</h2>
+                      <p>{result.description}</p>
+                    </div>
 
-    <form onSubmit={handleSubmit} className='h-screen w-full p-10 flex'>
-      {/* Input Picture */}
-      <label className="w-1/2 h-full border border-white bg-zinc-900 hover:bg-zinc-800 rounded-2xl p-4 text-white cursor-pointer flex items-center justify-center">
-        Choose Image
-        <input
-          name='uploadedImage'
-          type="file"
-          accept="image/*"
-          className="hidden"
-        />
-      </label>
-      <div className='w-1/2 h-full'>
-        {/* Interest Section */}
-        <div className='w-full h-1/3 flex flex-col gap-4 ps-4'>
-          <textarea name="interests" id="" placeholder='Enter interests seperated by Comma.' className='border rounded-2xl p-4 h-full' />
-          <button className='w-fit px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 cursor-pointer'>Submit</button>
-        </div>
-        <div className='ps-4 pt-4 h-1/2'>
-          {/* Result Window */}
-          <h1>Results:</h1>
-          <hr />
-          <div className='h-full overflow-y-auto'>
-            {
-              results ? results.map((result, index) => {
-                return (
-                  <div key={index} className='border rounded-lg p-4 my-4'>
-                    <h2 className='text-xl font-bold'>{result.title}</h2>
-                    <p>{result.description}</p>
-                  </div>
-
-                )
-              }) : <p>No results yet. Please upload an image and submit your interests.</p>
-            }
+                  )
+                }) : <p >No results yet. Please upload an image and submit your interests.</p>
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </section>
 
 
   )
